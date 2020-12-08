27f5952784b855071e28b4702c1b650aaef2e231
ejiro <ejiro1994@gmail.com>
2020-12-08 19:54:16 +0000

    3rd commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,44 +8,12 @@ export default function Home() {
     <div>
       <Helmet>
         <title>testetststststststs</title>
-        <meta
-          name='description'
-          content='this is the content part if this shows the helmet is working'
-        />
-        <meta 
-        property="og:url" 
-        content="https://stories.google/"
-         />
-        <meta 
-        property="og:title" 
-        content="Web Stories on Google" 
-
-        />
-        <meta 
-        name="description" 
-        content="The tappable story format has never been more accessible—to creators and readers alike. See what happens when Google brings stories to the open web."
-        />
-          <meta 
-          property="og:description" 
-          content="The tappable story format has never been more accessible—to creators and readers alike. See what happens when Google brings stories to the open web." 
-
-          />
-          <meta 
-          property="og:image" 
-          content="https://lh3.googleusercontent.com/jkRCEaBKzyVNijg12zycgjObWmQucwktNiscALqOZp5GL-0ZFG-I3AENfYXpHr8h_jrjng1sGbbrfX63O_Yg7-YToMejArZVUki8zw" 
-
-          />
-          <meta 
-          property="og:image:width" 
-          content="1200" 
-
-          />
-          <meta 
-          property="og:image:height" 
-          content="630" 
-
-          />
-
+        <meta name="og:title" content="The Rock" />
+        <meta name="og:type" content="movie" />
+        <meta name="og:url" content="http://www.ejirolennox.com" />
+        <meta name="og:image" content="https://lh3.googleusercontent.com/jkRCEaBKzyVNijg12zycgjObWmQucwktNiscALqOZp5GL-0ZFG-I3AENfYXpHr8h_jrjng1sGbbrfX63O_Yg7-YToMejArZVUki8zw" />
+        <meta name="og:site_name" content="IMDb" />
+        <meta name="og:description" content="A group of U.S. Marines, under command of..." />
       </Helmet>
         <div className="title">
           <Hero />
