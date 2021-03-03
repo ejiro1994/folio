128c35666db88a58053d6059d1afd5df437fe662
ejiro <ejiro1994@gmail.com>
2021-03-03 20:02:01 +0000

    title

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <div>
       <Helmet>
-        <title>testetststststststs</title>
-        <meta property="og:title" content="Creative Developer - Ejiro Lennox - London UK" />
+        <title>Ejiro Lennox</title>
+        <meta property="og:title" content="Ejiro Lennox - London UK" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="http://www.ejirolennox.com" />
         <meta property="og:image" content="https://lh3.googleusercontent.com/jkRCEaBKzyVNijg12zycgjObWmQucwktNiscALqOZp5GL-0ZFG-I3AENfYXpHr8h_jrjng1sGbbrfX63O_Yg7-YToMejArZVUki8zw" />
