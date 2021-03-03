3440476b07824fd6f7fb4cc895dd32813788e5c7
ejiro <ejiro1994@gmail.com>
2021-03-03 19:55:30 +0000

    website summary

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,11 @@ export default function Home() {
     <div>
       <Helmet>
         <title>testetststststststs</title>
-        <meta property="og:title" content="ejiro best website test works!" />
+        <meta property="og:title" content="Creative Developer - Ejiro Lennox - London UK" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="http://www.ejirolennox.com" />
         <meta property="og:image" content="https://lh3.googleusercontent.com/jkRCEaBKzyVNijg12zycgjObWmQucwktNiscALqOZp5GL-0ZFG-I3AENfYXpHr8h_jrjng1sGbbrfX63O_Yg7-YToMejArZVUki8zw" />
-        <meta name="og:description" content="A group of U.S. Marines, under command of..." />
+        <meta name="og:description" content="A multidisciplinary creative specialising in music production and web design. Based in London" />
         <meta property="og:locale" content="en_US" />
         <link rel="canonical" href=""/>
       </Helmet>
