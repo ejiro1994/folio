63c4a000876cca0e63fe5eea9e910eb86025eb38
ejiro <ejiro1994@gmail.com>
2021-03-04 04:40:15 +0000

    image for summary

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import EjImg from '../imgs/File_00112.png' 
 
 function Hero() {
 	return (
 		<div>
 			<h1>Ejiro Lennox - Music Production</h1>
-			<img src="../../" alt=""/>
+			<img src={EjImg} alt=""/>
+
 		</div>
 	)
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import "./index.scss"
 import Hero from "./Hero"
 import { Helmet } from 'react-helmet'
+import EjImg from '../imgs/File_00112.png' 
 
 export default function Home() {
   return (
@@ -11,8 +12,8 @@ export default function Home() {
         <meta property="og:title" content="Ejiro Lennox - London UK" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="http://www.ejirolennox.com" />
-        <meta property="og:image" content="https://lh3.googleusercontent.com/jkRCEaBKzyVNijg12zycgjObWmQucwktNiscALqOZp5GL-0ZFG-I3AENfYXpHr8h_jrjng1sGbbrfX63O_Yg7-YToMejArZVUki8zw" />
-        <meta name="og:description" content="A multidisciplinary creative specialising in music production and web design. Based in London" />
+        <meta property="og:image" content={EjImg} />
+        <meta name="og:description" content="A multidisciplinary creative specialising in music production and web design - Based in London" />
         <meta property="og:locale" content="en_US" />
         <link rel="canonical" href=""/>
       </Helmet>
